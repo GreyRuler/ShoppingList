1c2c3163f10ddeb108b237f6dd5333696033e2b6
GreyRuler <daniil_vasilenko757@mail.ru>
2022-10-09 14:07:56 +0300

    impl

---
+++ src/ts/Smartphone.ts
@@ -0,0 +1,27 @@
+import Product from './Product';
+
+export default class Smartphone implements Product {
+  readonly id: number;
+
+  readonly title: string;
+
+  readonly country: string;
+
+  readonly description: string;
+
+  readonly price: number;
+
+  constructor(
+    id: number,
+    title: string,
+    country: string,
+    description: string,
+    price: number,
+  ) {
+    this.id = id;
+    this.title = title;
+    this.country = country;
+    this.description = description;
+    this.price = price;
+  }
+}
